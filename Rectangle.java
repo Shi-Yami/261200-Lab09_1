aa38b06910e3cbcdc7c65b499f0902453d6f818d
Shi-Yami <aungthiha_1@cmu.ac.th>
2025-09-03 20:49:00 +0700

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,1 +1,22 @@
+package oop.ocp;
 
+public class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
+
+    // Getters and setters
+    public double getWidth() { return width; }
+    public double getHeight() { return height; }
+    public void setWidth(double width) { this.width = width; }
+    public void setHeight(double height) { this.height = height; }
+}
